62727c0683c43d5c96449606a8fda7c86ab21657
Cedric Jian N. Martin <cjnmartin.student@ua.edu.ph>
2025-02-06 13:06:47 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="name">Cedric Jian Martin</div>
+      <div className="box">
+        <div className="subject">CPEITEL</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
